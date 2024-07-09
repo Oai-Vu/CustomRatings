c098c126bdf1a785878ca789772f14753badf52e
OaiVu <oai.vq1@gmail.com>
2024-07-09 14:42:11 +0700

    Initial Commit

---
+++ CustomRatings/CustomRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRatingsApp.swift
+//  CustomRatings
+//
+//  Created by Vũ Quốc Oai on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
